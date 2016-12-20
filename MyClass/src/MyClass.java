24fe009b86a3a44cbe36f14f31bc14c5cc17347c
andrey_solovev (Andrey) <fr0ga@yandex.ru>
2016-12-20 15:20:55 +0300

    hello world!

--- MyClass/src/MyClass.java
+++ MyClass/src/MyClass.java
@@ -5,5 +5,6 @@ public class MyClass {
     public static void main(String[] args) {
         System.out.println("hello");
         System.out.println(123123);
+        System.out.println("world");
     }
 }
